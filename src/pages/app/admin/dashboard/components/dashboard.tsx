11ceca7dee6c016bff85cc0d40eee3505bab9db2
kasishs-dev <kasishs1311@gmail.com>
2025-02-23 20:25:24 +0530

    update dashboard ui

--- src/pages/app/admin/dashboard/components/dashboard.tsx
+++ src/pages/app/admin/dashboard/components/dashboard.tsx
@@ -1,8 +1,8 @@
-// Styles & Assets
+import React from "react";
 import styles from "./Dashboard.module.scss";
-import { GraduationCap, School, Users } from '../../../../../assets/icon/rooticon'
-
-// Type & Constants
+import {
+  CalendarCheck, School, Users, UserRound, ReceiptIndianRupee, Ticket, Bell, Goal, FileUser, FileTerminal, PencilLine, GraduationCap, CalendarRange, Inbox, Book, ClipboardList
+} from "lucide-react";
 interface Stat {
   title: string;
   value: number;
@@ -29,39 +29,110 @@ const stats: Stat[] = [
     growth: "+1.8% growth",
     icon: <GraduationCap className={styles.icon} />,
   },
-  { title: "Subjects", value: 30, growth: "+3.5% growth" },
-  { title: "Exams", value: 120, growth: "+4.7% growth" },
-  { title: "Events", value: 25, growth: "+6.3% growth" },
-  { title: "Students Actions", value: 0 },
-  { title: "Teachers Actions", value: 0 },
-  { title: "Classes Actions", value: 0 },
+  {
+    title: "Subjects",
+    value: 30,
+    growth: "+3.5% growth",
+    icon: <Book className={styles.icon} />, 
+  },
+  {
+    title: "Exams",
+    value: 120,
+    growth: "+4.7% growth",
+    icon: <ClipboardList className={styles.icon} />, 
+  },
+  {
+    title: "Events",
+    value: 25,
+    growth: "+6.3% growth",
+    icon: <CalendarCheck className={styles.icon} />,
+  },
+  {
+    title: "Fees",
+    value: 25,
+    growth: "+6.3% growth",
+    icon: <ReceiptIndianRupee className={styles.icon} />,
+  },{
+    title: "Hall Ticket",
+    value: 25,
+    growth: "+6.3% growth",
+    icon: <Ticket className={styles.icon} />,
+  },
+  {
+    title: "Notifications",
+    value: 25,
+    growth: "+6.3% growth",
+    icon: <Bell className={styles.icon} />,
+  },
+  {
+    title: "Placements",
+    value: 25,
+    growth: "+6.3% growth",
+    icon: <Goal className={styles.icon} />,
+  },
+  {
+    title: "Profile",
+    value: 25,
+    growth: "+6.3% growth",
+    icon: <FileUser className={styles.icon} />,
+  },
+  {
+    title: "Request",
+    value: 25,
+    growth: "+6.3% growth",
+    icon: <FileTerminal className={styles.icon} />,
+  },
+  {
+    title: "Result",
+    value: 25,
+    growth: "+6.3% growth",
+    icon: <PencilLine className={styles.icon} />,
+  },
+  {
+    title: "Time Table",
+    value: 25,
+    growth: "+6.3% growth",
+    icon: <CalendarRange className={styles.icon} />,
+  },
+  {
+    title: "Updates",
+    value: 25,
+    growth: "+6.3% growth",
+    icon: <Inbox className={styles.icon} />,
+  },{
+    title: "User",
+    value: 25,
+    growth: "+6.3% growth",
+    icon: <UserRound className={styles.icon} />,
+  },
 ];
 
-const Dashboard = () => {
+const Dashboard: React.FC = () => {
   return (
     <div className={styles.dashboard}>
-      <h1>Education Dashboard</h1>
+      <h1>Dashboard</h1>
 
       {/* Analytics Overview */}
       <section className={styles.analyticsOverview}>
         <h3>Analytics Overview</h3>
         <div className={styles.analyticsContainer}>
           <div>
-            <span>1,375</span>
+            <span>4582</span>
             <p>Total Students</p>
           </div>
           <div>
-            <span>3.7</span>
-            <p>Avg GPA</p>
+            <span>12</span>
+            <p>Total Institute</p>
           </div>
           <div>
-            <span>92%</span>
-            <p>Pass Rate</p>
+            <span>46</span>
+            <p>Total Classes</p>
           </div>
         </div>
       </section>
 
-      {/* Dynamic Stats Section */}
+      {/* Stats Section */}
+      <h3>Models</h3>
       <section className={styles.statsSection}>
         {stats.map((stat, index) => (
           <div key={index} className={styles.statCard}>
